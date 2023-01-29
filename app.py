d5f5c7c7aa276ecc601975ae2ee3b7a297b72bae
SaddamHasanov <123798376+SaddamHasanov@users.noreply.github.com>
2023-01-29 09:42:21 +0400

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from twilio.twiml.messaging_response import MessagingResponse
 from pymongo import MongoClient
 from datetime import datetime
 
-app = Flask(_name_)
-
-# Connect to MongoDB
 cluster = MongoClient("mongodb+srv://Saddam:Matrixgame213@wpbotdatabase."
                       "giznbjw.mongodb.net/?retryWrites=true&w=majority",
                       tls=True, tlsAllowInvalidCertificates=True)
@@ -15,72 +12,23 @@ users = db["users"]
 orders = db["orders"]
 
 app = Flask(__name__)
-orders = db["orders"]def handle_main_menu(number, option):
-if option == 1:
-return "0️⃣ Geri qayıtmaq\n"
-"1️⃣ Excel\n"
-"2️⃣ MOSE\n"
-"3️⃣ Power BI (PL-300)\n"
-"4️⃣ SQL (1Z0-071)\n"
-"5️⃣ Data Analitika", "kurslar"
-elif option == 2:
-return "Əlaqə nömrəsi: 050-369-60-88\n"
-"Email: seddamhasanov213@yandex.com", "main"
-elif option == 3:
-return "3 seçildi", "main"
-elif option == 4:
-return "4 seçildi", "main"
-else:
-return "Məlumat ala bilmək üçün zəhmət olmasa 1, 2, 3, 4\nrəqəmlərindən birini daxil edin", "main"
 
-def handle_kurslar_menu(number, option):
-if option == 0:
-return "1️⃣ Kurslar haqqında məlumat\n"
-"2️⃣ Bizimlə əlaqə\n"
-"3️⃣ FAQ\n"
-"4️⃣ Təlimlərimizin sillabusları\n", "main"
-elif 1 <= option <= 5:
-kurslar = ["Excel", "MOSE", "PowerBI (PL-300)", "SQL (1Z0-071)", "Data Analitika"]
-return f"{kurslar[option - 1]} haqqında məlumat", "main"
-else:
-return "Məlumat ala bilmək üçün zəhmət olmasa 0, 1, 2, 3, 4, 5\nrəqəmlərindən birini daxil edin", "main"
 
 @app.route("/", methods=['get', 'post'])
 def reply():
-def update_user_status(number, status):
-users.update_one({"number": number}, {"$set": {"status": status}})
 
     # fetching
     text = request.form.get('Body')
     number = request.form.get('From')
-@app.route("/", methods=['POST'])
-def sms():
-number = request.form['From']
-message_body = request.form['Body']user = users.find_one({"number": number})
-if user is None:
-    users.insert_one({"number": number, "status": "main"})
-    update_user_status(number, "main")
-    resp = MessagingResponse()
-    resp.message("Salam, nə istəyirsiniz?")
-    return str(resp)
 
     # changing
     text = str(text).lower()
-status = user["status"]
+    number = number.replace("whatsapp:", "")[:-2]
 
     # reply to messages
     response = MessagingResponse()
-if status == "main":
-    try:
-        option = int(message_body)
-        response_text, new_status ="POST"])
-def sms_reply():
-number = request.form['From']
-message_body = request.form['Body']
-user = users.find_one({"number": number})if user is None:
-    users.insert_one({"number": number, "status": "main"})
     user = users.find_one({"number": number})
-    if (bool(user) == False) or ('salam' in text):
+    if (bool(user) is False) or ('salam' in text):
         response.message("Salam, dəyərli müştərimiz, *Handex komandası* olaraq\nbütün "
                          "suallarınıza cavab verməyə və sizə dəstək olmağa hazırıq: \n\n"
                          "1️⃣ Kurslar haqqında məlumat\n2️⃣ Bizimlə əlaqə\n3️⃣ FAQ\n4️⃣ Təlimlərimizin sillabusları\n")
@@ -89,7 +37,7 @@ user = users.find_one({"number": number})if user is None:
     elif user["status"] == "main":
         try:
             option = int(text)
-        except:
+        except ValueError:
             response.message("Məlumat ala bilmək üçün zəhmət olmasa 1, 2, 3, 4\nrəqəmlərindən birini daxil edin")
             return flask.Response(str(response), mimetype="application/xml")
 
@@ -114,7 +62,7 @@ user = users.find_one({"number": number})if user is None:
     elif user["status"] == "kurslar":
         try:
             option = int(text)
-        except:
+        except ValueError:
             response.message("Məlumat ala bilmək üçün zəhmət olmasa 0, 1, 2, 3, 4, 5\nrəqəmlərindən birini daxil edin")
             return flask.Response(str(response), mimetype="application/xml")
         if option == 0:
@@ -154,31 +102,9 @@ user = users.find_one({"number": number})if user is None:
 
     users.update_one({"number": number}, {"$push": {"messages": {"text": text, "date": datetime.now()}}})
     return flask.Response(str(response), mimetype="application/xml")
-if user["status"] == "main":
-    try:
-        option = int(message_body)
-        response_text, next_status = handle_main_menu(number, option)
-    except ValueError:
-        response_text = "Məlumat ala bilmək üçün zəhmət olmasa 1, 2, 3, 4\nrəqəmlərindən birini daxil edin"
-        next_status = "main"
-elif user["status"] == "kurslar":
-    try:
-        option = int(message_body)
-        response_text, next_status = handle_kurslar_menu(number, option)
-    except ValueError:
-        response_text = "Məlumat ala bilmək üçün zəhmət olmasa 0, 1, 2, 3, 4, 5\nrəqəmlərindən birini daxil edin"
-        next_status = "kurslar"
-else:
-    response_text = "Səhv baş verdi, lütfən yenidən cəhd edin"
-    next_status = "main"
 
-update_user_status(number, next_status)
 
 if __name__ != "__main__":
     pass
 else:
     app.run()
-resp = MessagingResponse()
-resp.message(response_text)
-return str(resp) if name == "main":
-app.run()
