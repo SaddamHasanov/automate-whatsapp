c78fd09c37ca66c5ca3b1d2268453632b12e3889
SaddamHasanov <123798376+SaddamHasanov@users.noreply.github.com>
2023-02-02 07:40:08 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-# import flask
+import flask
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from pymongo import MongoClient
 from datetime import datetime
 
-cluster = MongoClient("mongodb+srv://Saddam:Matrixgame213@wpbotdatabase."
-                      "giznbjw.mongodb.net/?retryWrites=true&w=majority",
-                      tls=True, tlsAllowInvalidCertificates=True)
+cluster = MongoClient("mongodb+srv://Saddam:<password>@wpbotdatabase.giznbjw.mongodb.net/?retryWrites=true&w=majority", tls=True, tlsAllowInvalidCertificates=True)
+                      
 db = cluster["botDB"]
 users = db["users"]
 orders = db["orders"]
@@ -31,56 +30,53 @@ def reply():
     if (bool(user) is False) or ('salam' in text):
         users.insert_one({"number": number, "status": "main", "messages": []})
         users.update_one({"number": number}, {"$set": {"status": "main"}})
-        return response.message("Salam, dəyərli müştərimiz, *Handex komandası* olaraq\nbütün "
-                                "suallarınıza cavab verməyə və sizə dəstək olmağa hazırıq: \n\n"
-                                "1️⃣ Kurslar haqqında məlumat\n2️⃣ Bizimlə əlaqə\n3️⃣ FAQ\n4️⃣ "
-                                "Təlimlərimizin sillabusları\n")
-        # return flask.Response(str(response), mimetype="application/xml")
+        response.message("Salam, dəyərli müştərimiz, *Handex komandası* olaraq\nbütün "
+                         "suallarınıza cavab verməyə və sizə dəstək olmağa hazırıq: \n\n"
+                         "1️⃣ Kurslar haqqında məlumat\n2️⃣ Bizimlə əlaqə\n3️⃣ FAQ\n4️⃣ "
+                         "Təlimlərimizin sillabusları\n")
+        return flask.Response(str(response), mimetype="application/xml")
     elif user["status"] == "main":
         try:
             option = int(text)
         except ValueError:
-            return response.message("Məlumat ala bilmək üçün zəhmət olmasa 1, 2, 3, 4\n"
-                                    "rəqəmlərindən birini daxil edin")
-            # return flask.Response(str(response), mimetype="application/xml")
+            response.message("Məlumat ala bilmək üçün zəhmət olmasa 1, 2, 3, 4\nrəqəmlərindən birini daxil edin")
+            return flask.Response(str(response), mimetype="application/xml")
 
         if option == 1:
+            response.message("0️⃣ Geri qayıtmaq\n"
+                             "1️⃣ Excel\n"
+                             "2️⃣ MOSE\n"
+                             "3️⃣ Power BI (PL-300)\n"
+                             "4️⃣ SQL (1Z0-071)\n"
+                             "5️⃣ Data Analitika")
             users.update_one({"number": number}, {"$set": {"status": "kurslar"}})
-            return response.message("0️⃣ Geri qayıtmaq\n"
-                                    "1️⃣ Excel\n"
-                                    "2️⃣ MOSE\n"
-                                    "3️⃣ Power BI (PL-300)\n"
-                                    "4️⃣ SQL (1Z0-071)\n"
-                                    "5️⃣ Data Analitika")
-            # return flask.Response(str(response), mimetype="application/xml")
+            return flask.Response(str(response), mimetype="application/xml")
         elif option == 2:
-            return response.message("Əlaqə nömrəsi: 050-369-60-88\n"
-                                    "Email: seddamhasanov213@yandex.com")
-            # return flask.Response(str(response), mimetype="application/xml")
+            response.message("Əlaqə nömrəsi: 050-369-60-88\n"
+                             "Email: seddamhasanov213@yandex.com")
+            return flask.Response(str(response), mimetype="application/xml")
         elif option == 3:
-            return response.message("3 seçildi")
-            # return flask.Response(str(response), mimetype="application/xml")
+            response.message("3 seçildi")
+            return flask.Response(str(response), mimetype="application/xml")
         elif option == 4:
-            return response.message("4 seçildi")
-            # return flask.Response(str(response), mimetype="application/xml")
+            response.message("4 seçildi")
+            return flask.Response(str(response), mimetype="application/xml")
         else:
-            return response.message("Məlumat ala bilmək üçün zəhmət olmasa 1, 2, 3, 4\nrəqəmlərindən birini daxil edin")
-            # return flask.Response(str(response), mimetype="application/xml")
+            response.message("Məlumat ala bilmək üçün zəhmət olmasa 1, 2, 3, 4\nrəqəmlərindən birini daxil edin")
+            return flask.Response(str(response), mimetype="application/xml")
     elif user["status"] == "kurslar":
         try:
             option = int(text)
         except ValueError:
-            return response.message("Məlumat ala bilmək üçün zəhmət olmasa 0, 1, 2, 3, 4, 5\n"
-                                    "rəqəmlərindən birini daxil edin")
-            # return flask.Response(str(response), mimetype="application/xml")
-
+            response.message("Məlumat ala bilmək üçün zəhmət olmasa 0, 1, 2, 3, 4, 5\nrəqəmlərindən birini daxil edin")
+            return flask.Response(str(response), mimetype="application/xml")
         if option == 0:
             users.update_one({"number": number}, {"$set": {"status": "main"}})
-            return response.message("1️⃣ Kurslar haqqında məlumat\n"
-                                    "2️⃣ Bizimlə əlaqə\n"
-                                    "3️⃣ FAQ\n"
-                                    "4️⃣ Təlimlərimizin sillabusları\n")
-            # return flask.Response(str(response), mimetype="application/xml")
+            response.message("1️⃣ Kurslar haqqında məlumat\n"
+                             "2️⃣ Bizimlə əlaqə\n"
+                             "3️⃣ FAQ\n"
+                             "4️⃣ Təlimlərimizin sillabusları\n")
+            return flask.Response(str(response), mimetype="application/xml")
         elif 1 <= option <= 5:
             kurslar = ["Excel", "MOSE", "PowerBI (PL-300)", "SQL (1Z0-071)", "Data Analitika"]
             qiymetler = [175, 169, 449, 400, 849]
@@ -101,17 +97,16 @@ def reply():
                     'VI gün – saat 11:00-13:00']
             selected = kurslar[option-1]
             users.update_one({"number": number}, {"$set": {"item": selected}})
+            response.message(f'0️⃣ Əsas səhifəyə qayıtmaq üçün\n'
+                             f'{selected} kursumuz:\n'
+                             f'Qiymət: {qiymetler[option-1]} AZN\n'
+                             f'Təlimçimiz: {mellimler[option-1]}\n'
+                             f'Təlim vaxtları: {vaxt[option-1]}')
             orders.insert_one({"number": number, "item": selected, "address": text, "order_time": datetime.now()})
-            return response.message(f'0️⃣ Əsas səhifəyə qayıtmaq üçün\n'
-                                    f'{selected} kursumuz:\n'
-                                    f'Qiymət: {qiymetler[option-1]} AZN\n'
-                                    f'Təlimçimiz: {mellimler[option-1]}\n'
-                                    f'Təlim vaxtları: {vaxt[option-1]}')
-            # return flask.Response(str(response), mimetype="application/xml")
+            return flask.Response(str(response), mimetype="application/xml")
         else:
-            return response.message("Məlumat ala bilmək üçün zəhmət olmasa 0, 1, 2, 3, 4, 5\n"
-                                    "rəqəmlərindən birini daxil edin")
-            # return flask.Response(str(response), mimetype="application/xml")
+            response.message("Məlumat ala bilmək üçün zəhmət olmasa 0, 1, 2, 3, 4, 5\nrəqəmlərindən birini daxil edin")
+            return flask.Response(str(response), mimetype="application/xml")
 
     users.update_one({"number": number}, {"$push": {"messages": {"text": text, "date": datetime.now()}}})
 
@@ -119,4 +114,4 @@ def reply():
 if __name__ != "__main__":
     pass
 else:
-    app.run(port=5000)
+    app.run()
